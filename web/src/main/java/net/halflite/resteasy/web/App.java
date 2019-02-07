ea74f8ab308a231465db494e7c7c4a1b02dc1aa7
Shingo Hayashibara <shingo@halflite.net>
2019-02-07 23:00:40 +0900

    jetty server spin-up

--- web/src/main/java/net/halflite/resteasy/web/App.java
+++ web/src/main/java/net/halflite/resteasy/web/App.java
@@ -1,13 +1,25 @@
 package net.halflite.resteasy.web;
 
+import org.eclipse.jetty.server.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Hello world!
+ * App Main
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    
+    public static void main(String[] args) {
+        try {
+            Server server = new Server(8080);
+            
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            LOGGER.warn(e.getMessage(), e);
+        }
     }
 }
